ac930b92d523d5c1486fd04fb8add53a186b16bd
CristianoAltomonte <cristianoaltomonte@hotmail.it>
2022-10-24 20:48:32 +0100

    Esercizio Dadi

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,38 @@ function iscriviti(){
 
 }
 
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// scrivere quante partite ha vinto il giocatore
+// scrivere quante partite ha vinto il computer
+
+let vittoriaplayer = 0;
+let vittoriacomputer = 0;
+
+let player = document.getElementById("player");
+let computer = document.getElementById("computer");
+
+
+function TiraDado(){
+    
+    let risultato = document.getElementById('risultato');
+    let randomplayer = Math.floor(Math.random() * 6 + 1);
+    let randomcomputer = Math.floor(Math.random() * 6 + 1);
+    
+    if (randomplayer > randomcomputer) {
+        risultato.innerHTML = `Hai vinto! il tuo numero è ${randomplayer}`;
+        vittoriaplayer = vittoriaplayer + 1;
+
+    } else if (randomplayer == randomcomputer) {
+        risultato.innerHTML = `Pareggio! ${randomplayer} e ${randomcomputer}! `;
+
+    } else {
+        risultato.innerHTML = `Hai perso ho fatto ${randomcomputer}, Rigiochiamo?`;
+        vittoriacomputer = vittoriacomputer + 1;
+    }
+
+    player.innerHTML = `Vittorie: ${vittoriaplayer}`;
+    computer.innerHTML = `Perdite: ${vittoriacomputer}`;
+
+}
